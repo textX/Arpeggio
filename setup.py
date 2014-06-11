f4420b037ce0cc26a516faf8e09c34e43136afce
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-06-12 00:45:15 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 ###############################################################################
 
 __author__ = "Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>"
-__version__ = "0.6"
+__version__ = "0.6.1"
 
 from setuptools import setup
 

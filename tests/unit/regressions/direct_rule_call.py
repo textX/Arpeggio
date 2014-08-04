378fbe440cf0a0706f852fa573a5348ead8ddee4
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-08-04 16:45:55 +0200

    Migrated one regression test to pytest

--- tests/unit/regressions/direct_rule_call.py
+++ tests/unit/regressions/direct_rule_call.py
@@ -1,39 +1,29 @@
-'''
-Created on Jul 6, 2014
-
-@author: igor
-'''
-import unittest
 from arpeggio import SemanticAction, ParserPython
 
+def test_direct_rule_call():
+    '''
+    Test regression where in direct rule call semantic action is
+    erroneously attached to both caller and callee.
+    '''
 
-class Test(unittest.TestCase):
+    def grammar():  return rule1, rule2
+    def rule1():    return "a"
+    def rule2():    return rule1
 
-    def test_direct_rule_call(self):
-        '''
-        Test regression where in direct rule call semantic action is
-        erroneously attached to both caller and callee.
-        '''
+    call_count = [0]
 
-        def grammar():  return rule1, rule2
-        def rule1():    return "a"
-        def rule2():    return rule1
+    class DummySemAction(SemanticAction):
+        def first_pass(self, parser, node, nodes):
+            call_count[0] += 1
+            return SemanticAction.first_pass(self, parser, node, nodes)
 
-        call_count = [0]
+    # Sem action is attached to rule2 only but
+    # this bug will attach it to rule1 also resulting in
+    # wrong call count.
+    rule2.sem = DummySemAction()
 
-        class DummySemAction(SemanticAction):
-            def first_pass(self, parser, node, nodes):
-                call_count[0] += 1
-                return SemanticAction.first_pass(self, parser, node, nodes)
+    parser = ParserPython(grammar)
+    parse_tree = parser.parse("aa")
+    parser.getASG()
 
-        # Sem action is attached to rule2 only but
-        # this bug will attach it to rule1 also resulting in
-        # wrong call count.
-        rule2.sem = DummySemAction()
-
-        parser = ParserPython(grammar)
-        parse_tree = parser.parse("aa")
-        parser.getASG()
-
-        self.assertEqual(call_count[0], 1,
-                         "Semantic action should be called once!")
+    assert call_count[0] == 1, "Semantic action should be called once!"

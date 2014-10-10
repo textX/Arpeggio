ed61b6434cf9a8ff21c574e8e4066902db568396
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-10-11 00:46:25 +0200

    Changed comment rule in cleanpeg to python/bash style (#).

--- arpeggio/cleanpeg.py
+++ arpeggio/cleanpeg.py
@@ -48,7 +48,7 @@ def rule_name():        return _(r"[a-zA-Z_]([a-zA-Z_]|[0-9])*")
 def rule_crossref():    return rule_name
 #def literal():          return [_(r"\'(\\\'|[^\'])*\'"),_(r'"[^"]*"')]
 def str_match():        return _(r'(\'(\\\'|[^\'])*\')|("[^"]*")')
-def comment():          return "//", _(".*\n")
+def comment():          return _("#.*\n")
 
 # ------------------------------------------------------------------
 # PEG Semantic Actions

c0c213dc3f7b83161483a7b4dc711cce24f9bd26
Igor Dejanovic <igor.dejanovic@gmail.com>
2015-02-10 10:04:18 +0100

    Version change.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 ###############################################################################
 
 __author__ = "Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>"
-__version__ = "0.10-dev"
+__version__ = "0.10"
 
 from setuptools import setup
 

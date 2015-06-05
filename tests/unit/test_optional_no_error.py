193b8f7fa3e889d417678b557a45db642432d7a9
Igor Dejanovic <igor.dejanovic@gmail.com>
2015-06-05 19:41:49 +0200

    Added test that verify that optional matches do not produce parsing errors.

---
+++ tests/unit/test_optional_no_error.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#######################################################################
+# Name: test_optional_no_error
+# Purpose: Test that failures in optional matches are not reported as errors.
+# Author: Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>
+# Copyright: (c) 2015 Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>
+# License: MIT License
+#######################################################################
+from __future__ import unicode_literals
+import pytest
+
+from arpeggio import ZeroOrMore, Optional, ParserPython, NoMatch
+
+
+def test_optional_no_error():
+    """
+    """
+    def grammar():  return Optional('a'), 'b'
+
+    parser = ParserPython(grammar)
+
+    try:
+        parser.parse('c')
+        assert False
+
+    except NoMatch as e:
+        assert "Expected 'b'" in str(e)

52ea2ddd7edc29eaa325d0cd488587cbacfb199c
Igor Dejanovic <igor.dejanovic@gmail.com>
2015-10-26 10:57:19 +0100

    Added semantic analysis to csv_peg example

--- examples/csv/csv_peg.py
+++ examples/csv/csv_peg.py
@@ -2,11 +2,13 @@
 # Name: csv.py
 # Purpose: Implementation of CSV parser in arpeggio.
 # Author: Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
-# Copyright: (c) 2014 Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
+# Copyright: (c) 2014-2015 Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
 # License: MIT License
 ##############################################################################
 
+from arpeggio import visit_parse_tree
 from arpeggio.cleanpeg import ParserPEG
+from csv import CSVVisitor
 
 def main(debug=False):
     # First we will make a parser - an instance of the CVS parser model.
@@ -21,6 +23,10 @@ def main(debug=False):
     # Creating parse tree out of textual input
     test_data = open('test_data.csv', 'r').read()
     parse_tree = parser.parse(test_data)
+
+    # Create list of lists using visitor
+    csv_content = visit_parse_tree(parse_tree, CSVVisitor())
+    print(csv_content)
 
 if __name__ == "__main__":
     # In debug mode dot (graphviz) files for parser model

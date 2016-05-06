5f7811860ad55093109c87fb748a806472633160
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-05-06 02:15:01 +0200

    Test sequence suppression.

---
+++ tests/unit/test_suppression.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#######################################################################
+# Name: test_suppression
+# Purpose: Test suppresion of parse tree nodes.
+# Author: Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>
+# Copyright: (c) 2016 Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>
+# License: MIT License
+#######################################################################
+
+from __future__ import unicode_literals
+import pytest
+from arpeggio import ParserPython, Sequence
+
+
+def test_sequence_suppress():
+    """
+    """
+
+    def grammar():     return Sequence("one", "two", "three", suppress=True), "four"
+
+    parser = ParserPython(grammar)
+
+    result = parser.parse("one two three four")
+    assert result[0] == "four"

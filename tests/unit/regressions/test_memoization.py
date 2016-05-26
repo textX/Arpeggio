3ba89ef00ae1c3c98055058b978368f6f27af752
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-05-26 23:55:52 +0200

    updated memoization tests.

--- tests/unit/regressions/test_memoization.py
+++ tests/unit/regressions/test_memoization.py
@@ -23,10 +23,12 @@ def test_memoization_positive(capsys):
     # Parse input where a rule1 will match but ruleb will fail
     # Second sequence will try rule1 again on the same location
     # and result should be found in the cache.
-    parse_tree = parser.parse("a b c")
+    parse_tree = parser.parse("a   b   c")
 
     # Assert that cached result is used
     assert  "Cache hit" in capsys.readouterr()[0]
+    assert parser.cache_hits == 1
+    assert parser.cache_misses == 4
 
 def test_memoization_nomatch(capsys):
     '''
@@ -43,6 +45,8 @@ def test_memoization_nomatch(capsys):
     parser = ParserPython(grammar, memoization=True, debug=True)
     parse_tree = parser.parse("c")
 
-    assert  "Cache hit for [rule1=Sequence, 0] = 'Expected rulea at position (1, 1)" in capsys.readouterr()[0]
+    assert  "Cache hit for [rule1=Sequence, 0] = '0'" in capsys.readouterr()[0]
+    assert parser.cache_hits == 1
+    assert parser.cache_misses == 4
 
 

9e740f61e2718ff4a796004ad9182ff1dd9a7de4
bitranox <rnowotny1966@gmail.com>
2020-05-15 18:34:09 +0200

    refractor

--- conftest.py
+++ conftest.py
@@ -26,5 +26,4 @@ def pytest_cmdline_preparse(args):
     if sys.version_info <= (3, 5):
         args[:] = ["--pep8"] + args
     else:
-        # changed from --codestyle to --pycodestyle in 2019-11-20
         args[:] = ["--pycodestyle"] + args

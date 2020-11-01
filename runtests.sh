b3b2c8d47034205c6bf5dc7054f19851c26b82ea
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-11-01 16:28:22 +0100

    Fix coverage run

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run all tests and generate coverage report
 
-coverage run --source arpeggio -m py.test arpeggio/tests || exit 1
+coverage run --omit="arpeggio/tests/*" --source arpeggio -m py.test arpeggio/tests || exit 1
 coverage report --fail-under 90 || exit 1
 # Run this to generate html report
 # coverage html --directory=coverage

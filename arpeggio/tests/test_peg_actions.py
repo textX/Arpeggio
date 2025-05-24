2c77a247e9a2ee43084d8d8672c191099283a362
Andrey Dotsenko <9fcc.dev@gmail.com>
2025-05-24 18:53:11 +0300

    Added a test for the previously added parser actions

---
+++ arpeggio/tests/test_peg_actions.py
@@ -0,0 +1,66 @@
+#######################################################################
+# Name: test_peg_parser
+# Purpose: Test for parser constructed using PEG textual grammars.
+# Authors: Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>, Andrey N. Dotsenko <pgandrey@yandex.ru>
+# Copyright: (c) 2025
+#           Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>,
+#           Andrey N. Dotsenko <pgandrey@yandex.ru>
+# License: MIT License
+#
+# This file is originally based on test_peg_parser.py
+#######################################################################
+
+import pytest
+from arpeggio.peg import ParserPEG
+from arpeggio.cleanpeg import ParserPEG as ParserPEGClean
+
+grammar = r'''
+parser_entry <- program_element* EOF;
+
+program_element <-
+    function
+    / function_call;
+
+function <-
+    FUNCTION_START function_name{push}
+    program_element*
+    FUNCTION_END function_name{pop};
+
+function_call <-
+    function_name
+    ARGUMENTS_START
+    VALID_NAME?
+    (
+        ARGUMENTS_DELIMITER
+        VALID_NAME
+    )*
+    ARGUMENTS_DELIMITER?
+    ARGUMENTS_END;
+
+FUNCTION_START <- 'def';
+function_name <- VALID_NAME;
+FUNCTION_END <- 'end of';
+ARGUMENTS_START <- '(';
+ARGUMENTS_END <- ')';
+VALID_NAME <- r'[a-zA-Z0-9_]+';
+ARGUMENTS_DELIMITER <- ',';
+'''
+
+clean_grammar = grammar.replace('<-', '=').replace(';', '')
+
+@pytest.mark.parametrize('parser', [
+    ParserPEGClean(clean_grammar, 'parser_entry'),
+    ParserPEG(grammar, 'parser_entry'),
+])
+def test_backreference(parser):
+    input = """
+def function_name1
+end of function_name1
+
+def function_name2
+    function_name1(arg1)
+end of function_name2
+
+function_name(1, 2, 3)
+    """
+    result = parser.parse(input)
